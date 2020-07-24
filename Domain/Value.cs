4687d2ca764ed88717cdb5a52c59bcf023c4355d
Macy Parker <macyvparker@gmail.com>
2020-07-24 17:54:26 -0400

    Added First Domain: ValuesController and Value

---
+++ Domain/Value.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace Domain
+{
+    public class Value
+    {
+        // Entity Framework is convention based
+        // When we give a property inside one of our entities the name of ID
+        // it is automatically used as the primary key
+        // database will automatically generate a number
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
