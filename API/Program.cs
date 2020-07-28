735b7a6d78768481d05425147149b6b703639b45
Macy Parker <macyvparker@gmail.com>
2020-07-28 16:57:49 -0400

    Created the database

--- API/Program.cs
+++ API/Program.cs
@@ -6,6 +6,9 @@ using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.DependencyInjection;
+using Persistence;
+using Microsoft.EntityFrameworkCore;
 
 namespace API
 {
@@ -13,7 +16,28 @@ namespace API
     {
         public static void Main(string[] args)
         {
-            CreateHostBuilder(args).Build().Run();
+            var host = CreateHostBuilder(args).Build();
+
+            // Get data context service while executing main method
+            // and dispose of it after method is executing by using
+            // the "using" method
+            using (var scope = host.Services.CreateScope()) {
+                var services = scope.ServiceProvider;
+                try 
+                {
+                    var context = services.GetRequiredService<DataContext>();
+                    // Applies pending migrations for the context to the database
+                    // and creates database if it does not already exist
+                    context.Database.Migrate();
+                }
+                catch(Exception ex) 
+                {
+                    var logger = services.GetRequiredService<ILogger<Program>>();
+                    logger.LogError(ex, "an error occured during migration");
+                }
+            }
+
+            host.Run(); // Runs our application
         }
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
