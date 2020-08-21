ac084de72c1aed44370f05dd1b15ab1e9be2c741
Macy Parker <macyvparker@gmail.com>
2020-08-21 17:57:52 -0400

    fetching data from the API in our React app

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -1,26 +1,42 @@
-import React from 'react';
+import React, {Component} from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+class App extends Component {
+  // provides data for the view when we show it in the browser
+  state = {
+    values: []
+  }
+
+  // trigger a re render of our components
+  // axios get returns an AxiosResponse
+  componentDidMount() {
+    axios.get('http://localhost:5000/api/values')
+      .then((response) =>
+      {
+        console.log(response);
+        this.setState({
+          values: response.data
+        })
+      });
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <header className="App-header">
+          <img src={logo} className="App-logo" alt="logo" />
+          <ul>
+            {this.state.values.map((value: any) => (
+              <li>{value.name}</li>
+            ))}
+          </ul>
+        </header>
+      </div>
+    );
+  }
+  
 }
 
 export default App;
