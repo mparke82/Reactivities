1ae02ddb60779877163093ba0642b7f4ec679bde
Macy Parker <macyvparker@gmail.com>
2020-09-10 17:25:54 -0400

    End of Section 3: walking skeleton - client

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -16,7 +16,6 @@ class App extends Component {
     axios.get('http://localhost:5000/api/values')
       .then((response) =>
       {
-        console.log(response);
         this.setState({
           values: response.data
         })
